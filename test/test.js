f9ab70f49c1303e54a156ee96ac00d82060b43d5
Kevin Johnson <kjjuno@gmail.com>
2019-02-10 10:36:19 -0700

    Adding the framework for code coverage

--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
 var assert = require('assert');
+var hello = require('../src/swagger-min');
 
 describe('fake test', function() {
     it('is a fake test', function() {
         assert.equal(1, 1);
     });
+  it('says hello', function() {
+    assert.equal('hello', hello());
+  });
 });
